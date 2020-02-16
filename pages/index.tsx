cc24fb59496da6af0806545a9a1b19d6f7570f90
Emanuele Manco <hello@emanuelemanco.com>
2020-02-16 17:41:38 +0000

    Added appbar and main container framework.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,123 @@
 import React from "react";
-import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
+
+import {createMuiTheme, createStyles, makeStyles, MuiThemeProvider, Theme} from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+
+import { Hidden } from '@material-ui/core';
+
 import Schedule from "./schedule";
+import {blue} from "@material-ui/core/colors";
+import Paper from "@material-ui/core/Paper";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }),
+);
+
 
 export default function Index() {
+
+  const classes = useStyles();
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAuth(event.target.checked);
+  };
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const theme = createMuiTheme({ palette: { type: "light", primary: blue } });
+
   return (
-    <Container>
-      <Box>
-        <Schedule/>
-      </Box>
-    </Container>
+    <MuiThemeProvider theme={theme}>
+      <Paper>
+        <div className={classes.root}>
+          <Hidden xsUp>
+            <FormGroup>
+              <FormControlLabel
+                control={<Switch checked={auth} onChange={handleChange} aria-label="login switch" />}
+                label={auth ? 'Logout' : 'Login'}
+              />
+            </FormGroup>
+          </Hidden>
+
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" className={classes.title}>
+                TodoYogi
+              </Typography>
+              {auth && (
+                <div>
+                  <IconButton
+                    aria-label="account of current user"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                    onClick={handleMenu}
+                    color="inherit"
+                  >
+                    <AccountCircle />
+                  </IconButton>
+                  <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorEl}
+                    anchorOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right',
+                    }}
+                    open={open}
+                    onClose={handleClose}
+                  >
+                    <MenuItem onClick={handleClose}>Profile</MenuItem>
+                    <MenuItem onClick={handleClose}>My account</MenuItem>
+                  </Menu>
+                </div>
+              )}
+            </Toolbar>
+          </AppBar>
+        </div>
+
+        <Box maxWidth="xl">
+          <Schedule/>
+        </Box>
+
+      </Paper>
+    </MuiThemeProvider>
+
   );
 }
