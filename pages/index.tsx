0c0deeb56eb26db0696b5461335a8c0d50d502cb
emanco <manu.manco@gmail.com>
2020-02-22 20:38:58 +0000

    Fixed sidebar transition.

--- pages/index.tsx
+++ pages/index.tsx
@@ -81,7 +81,7 @@ const useStyles = makeStyles((theme: Theme) =>
     content: {
       flexGrow: 1,
       padding: theme.spacing(1),
-      transition: theme.transitions.create('margin', {
+      transition: theme.transitions.create(['margin', 'width'], {
         easing: theme.transitions.easing.sharp,
         duration: theme.transitions.duration.leavingScreen,
       }),
@@ -90,7 +90,7 @@ const useStyles = makeStyles((theme: Theme) =>
         marginLeft: 0
     },
     contentShift: {
-      transition: theme.transitions.create('margin', {
+      transition: theme.transitions.create(['margin', 'width'], {
         easing: theme.transitions.easing.easeOut,
         duration: theme.transitions.duration.enteringScreen,
       }),
