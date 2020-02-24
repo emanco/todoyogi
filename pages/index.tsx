68b9cd2f18f5f6a7b6b363cbc63aedc014ff93be
Emanuele Manco <hello@emanuelemanco.com>
2020-02-24 08:48:33 +0000

    Added tabs components.

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import Box from '@material-ui/core/Box';
 
 import {createMuiTheme, createStyles, makeStyles, MuiThemeProvider, Theme} from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
@@ -18,22 +20,57 @@ import { Hidden } from '@material-ui/core';
 
 import clsx from 'clsx';
 import Drawer from '@material-ui/core/Drawer';
-import List from '@material-ui/core/List';
+// import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+// import ListItem from '@material-ui/core/ListItem';
+// import ListItemIcon from '@material-ui/core/ListItemIcon';
+// import ListItemText from '@material-ui/core/ListItemText';
+// import InboxIcon from '@material-ui/icons/MoveToInbox';
+// import MailIcon from '@material-ui/icons/Mail';
+
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 
 import Schedule from "./schedule";
 import {blue} from "@material-ui/core/colors";
 import Paper from "@material-ui/core/Paper";
 
-const drawerWidth = 240;
+const drawerWidth = 350;
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: any;
+  value: any;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box p={3}>{children}</Box>}
+    </Typography>
+  );
+}
+
+function a11yProps(index: any) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -77,6 +114,23 @@ const useStyles = makeStyles((theme: Theme) =>
       padding: theme.spacing(0, 1),
       ...theme.mixins.toolbar,
       justifyContent: 'flex-end',
+    },
+    tab: {
+      minWidth: 72,
+      fontWeight: theme.typography.fontWeightRegular,
+      fontSize: theme.typography.pxToRem(15),
+      textTransform: 'initial'
+    },
+    tabPanel: {
+      padding: 0,
+    },
+    heading: {
+      fontSize: theme.typography.pxToRem(15),
+      fontWeight: theme.typography.fontWeightRegular,
+    },
+    secondaryHeading: {
+      fontSize: theme.typography.pxToRem(15),
+      color: theme.palette.text.secondary,
     },
     content: {
       flexGrow: 1,
@@ -129,6 +183,23 @@ export default function Index() {
 
   const handleClose = () => {
     setAnchorEl(null);
+  };
+
+
+  const [value, setValue] = React.useState(0);
+
+  const handleTabChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue);
+
+    console.log(event);
+  };
+
+
+  const [expanded, setExpanded] = React.useState<string | false>(false);
+
+  const handleAccordionChange = (panel: string) => (event: React.ChangeEvent<{}>, isExpanded: boolean) => {
+    setExpanded(isExpanded ? panel : false);
+    console.log(event);
   };
 
   const theme = createMuiTheme({ palette: { type: "light", primary: blue } });
@@ -206,29 +277,106 @@ export default function Index() {
                     paper: classes.drawerPaper,
                 }}
             >
+
+
+
                 <div className={classes.drawerHeader}>
                     <IconButton onClick={handleDrawerClose}>
                         {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
                     </IconButton>
                 </div>
                 <Divider />
-                <List>
-                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                        <ListItem button key={text}>
-                            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                            <ListItemText primary={text} />
-                        </ListItem>
-                    ))}
-                </List>
-                <Divider />
-                <List>
-                    {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                        <ListItem button key={text}>
-                            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                            <ListItemText primary={text} />
-                        </ListItem>
-                    ))}
-                </List>
+
+                <Paper square>
+                  <Tabs value={value} onChange={handleTabChange} aria-label="simple tabs example" indicatorColor="primary" textColor="primary">
+                    <Tab className={classes.tab} label="Space/Project" {...a11yProps(0)} disableRipple/>
+                    <Tab className={classes.tab} label="Status" {...a11yProps(1)} disableRipple/>
+                  </Tabs>
+                </Paper>
+
+                <TabPanel value={value} index={0}>
+                  <ExpansionPanel className={classes.tabPanel} expanded={expanded === 'panel1'} onChange={handleAccordionChange('panel1')}>
+                    <ExpansionPanelSummary
+                      expandIcon={<ExpandMoreIcon />}
+                      aria-controls="panel1bh-content"
+                      id="panel1bh-header"
+                    >
+                      <Typography className={classes.heading}>General settings</Typography>
+                    </ExpansionPanelSummary>
+                    <ExpansionPanelDetails>
+                      <Typography>
+                        Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat. Aliquam eget
+                        maximus est, id dignissim quam.
+                      </Typography>
+                    </ExpansionPanelDetails>
+                  </ExpansionPanel>
+                  <ExpansionPanel expanded={expanded === 'panel2'} onChange={handleAccordionChange('panel2')}>
+                    <ExpansionPanelSummary
+                      expandIcon={<ExpandMoreIcon />}
+                      aria-controls="panel2bh-content"
+                      id="panel2bh-header"
+                    >
+                      <Typography className={classes.heading}>Users</Typography>
+                    </ExpansionPanelSummary>
+                    <ExpansionPanelDetails>
+                      <Typography>
+                        Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus, varius pulvinar
+                        diam eros in elit. Pellentesque convallis laoreet laoreet.
+                      </Typography>
+                    </ExpansionPanelDetails>
+                  </ExpansionPanel>
+                  <ExpansionPanel expanded={expanded === 'panel3'} onChange={handleAccordionChange('panel3')}>
+                    <ExpansionPanelSummary
+                      expandIcon={<ExpandMoreIcon />}
+                      aria-controls="panel3bh-content"
+                      id="panel3bh-header"
+                    >
+                      <Typography className={classes.heading}>Advanced settings</Typography>
+                    </ExpansionPanelSummary>
+                    <ExpansionPanelDetails>
+                      <Typography>
+                        Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas eros,
+                        vitae egestas augue. Duis vel est augue.
+                      </Typography>
+                    </ExpansionPanelDetails>
+                  </ExpansionPanel>
+                  <ExpansionPanel expanded={expanded === 'panel4'} onChange={handleAccordionChange('panel4')}>
+                    <ExpansionPanelSummary
+                      expandIcon={<ExpandMoreIcon />}
+                      aria-controls="panel4bh-content"
+                      id="panel4bh-header"
+                    >
+                      <Typography className={classes.heading}>Personal data</Typography>
+                    </ExpansionPanelSummary>
+                    <ExpansionPanelDetails>
+                      <Typography>
+                        Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas eros,
+                        vitae egestas augue. Duis vel est augue.
+                      </Typography>
+                    </ExpansionPanelDetails>
+                  </ExpansionPanel>
+                </TabPanel>
+                <TabPanel value={value} index={1}>
+                  Item Two
+                </TabPanel>
+
+                {/*<List>*/}
+                {/*    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (*/}
+                {/*        <ListItem button key={text}>*/}
+                {/*            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>*/}
+                {/*            <ListItemText primary={text} />*/}
+                {/*        </ListItem>*/}
+                {/*    ))}*/}
+                {/*</List>*/}
+                {/*<Divider />*/}
+                {/*<List>*/}
+                {/*    {['All mail', 'Trash', 'Spam'].map((text, index) => (*/}
+                {/*        <ListItem button key={text}>*/}
+                {/*            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>*/}
+                {/*            <ListItemText primary={text} />*/}
+                {/*        </ListItem>*/}
+                {/*    ))}*/}
+                {/*</List>*/}
             </Drawer>
 
         </div>
