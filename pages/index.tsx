fba871d96a53b9605fd21cff19b6fc31e3a2d9ee
Emanuele Manco <hello@emanuelemanco.com>
2020-02-29 16:52:49 +0000

    Refactored accordion component.

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,9 +31,12 @@ import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 // import MailIcon from '@material-ui/icons/Mail';
 
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelActions from '@material-ui/core/ExpansionPanelActions';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Chip from '@material-ui/core/Chip';
+import Button from '@material-ui/core/Button';
 
 
 import Schedule from "./schedule";
@@ -101,6 +104,28 @@ const useStyles = makeStyles((theme: Theme) =>
     hide: {
       display: 'none',
     },
+    icon: {
+      verticalAlign: 'bottom',
+      height: 20,
+      width: 20,
+    },
+    details: {
+      alignItems: 'center',
+    },
+    column: {
+      flexBasis: '33.33%',
+    },
+    helper: {
+      borderLeft: `2px solid ${theme.palette.divider}`,
+      padding: theme.spacing(1, 2),
+    },
+    link: {
+      color: theme.palette.primary.main,
+      textDecoration: 'none',
+      '&:hover': {
+        textDecoration: 'underline',
+      },
+    },
     drawer: {
       width: drawerWidth,
       flexShrink: 0,
@@ -124,9 +149,16 @@ const useStyles = makeStyles((theme: Theme) =>
     tabPanel: {
       padding: 0,
     },
+    tabs: {
+      marginLeft: '-14px',
+      marginRight: '-14px',
+      marginTop: '-14px',
+      marginBottom: '-14px',
+    },
     heading: {
       fontSize: theme.typography.pxToRem(15),
-      fontWeight: theme.typography.fontWeightRegular,
+      flexBasis: '50%',
+      flexShrink: 0,
     },
     secondaryHeading: {
       fontSize: theme.typography.pxToRem(15),
@@ -191,14 +223,6 @@ export default function Index() {
   const handleTabChange = (event: React.ChangeEvent<{}>, newValue: number) => {
     setValue(newValue);
 
-    console.log(event);
-  };
-
-
-  const [expanded, setExpanded] = React.useState<string | false>(false);
-
-  const handleAccordionChange = (panel: string) => (event: React.ChangeEvent<{}>, isExpanded: boolean) => {
-    setExpanded(isExpanded ? panel : false);
     console.log(event);
   };
 
@@ -295,66 +319,45 @@ export default function Index() {
                 </Paper>
 
                 <TabPanel value={value} index={0}>
-                  <ExpansionPanel className={classes.tabPanel} expanded={expanded === 'panel1'} onChange={handleAccordionChange('panel1')}>
-                    <ExpansionPanelSummary
-                      expandIcon={<ExpandMoreIcon />}
-                      aria-controls="panel1bh-content"
-                      id="panel1bh-header"
-                    >
-                      <Typography className={classes.heading}>General settings</Typography>
-                    </ExpansionPanelSummary>
-                    <ExpansionPanelDetails>
-                      <Typography>
-                        Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat. Aliquam eget
-                        maximus est, id dignissim quam.
-                      </Typography>
-                    </ExpansionPanelDetails>
-                  </ExpansionPanel>
-                  <ExpansionPanel expanded={expanded === 'panel2'} onChange={handleAccordionChange('panel2')}>
-                    <ExpansionPanelSummary
-                      expandIcon={<ExpandMoreIcon />}
-                      aria-controls="panel2bh-content"
-                      id="panel2bh-header"
-                    >
-                      <Typography className={classes.heading}>Users</Typography>
-                    </ExpansionPanelSummary>
-                    <ExpansionPanelDetails>
-                      <Typography>
-                        Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus, varius pulvinar
-                        diam eros in elit. Pellentesque convallis laoreet laoreet.
-                      </Typography>
-                    </ExpansionPanelDetails>
-                  </ExpansionPanel>
-                  <ExpansionPanel expanded={expanded === 'panel3'} onChange={handleAccordionChange('panel3')}>
-                    <ExpansionPanelSummary
-                      expandIcon={<ExpandMoreIcon />}
-                      aria-controls="panel3bh-content"
-                      id="panel3bh-header"
-                    >
-                      <Typography className={classes.heading}>Advanced settings</Typography>
-                    </ExpansionPanelSummary>
-                    <ExpansionPanelDetails>
-                      <Typography>
-                        Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas eros,
-                        vitae egestas augue. Duis vel est augue.
-                      </Typography>
-                    </ExpansionPanelDetails>
-                  </ExpansionPanel>
-                  <ExpansionPanel expanded={expanded === 'panel4'} onChange={handleAccordionChange('panel4')}>
-                    <ExpansionPanelSummary
-                      expandIcon={<ExpandMoreIcon />}
-                      aria-controls="panel4bh-content"
-                      id="panel4bh-header"
-                    >
-                      <Typography className={classes.heading}>Personal data</Typography>
-                    </ExpansionPanelSummary>
-                    <ExpansionPanelDetails>
-                      <Typography>
-                        Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas eros,
-                        vitae egestas augue. Duis vel est augue.
-                      </Typography>
-                    </ExpansionPanelDetails>
-                  </ExpansionPanel>
+                  <div className="tabs">
+                    <ExpansionPanel defaultExpanded>
+                      <ExpansionPanelSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1c-content"
+                        id="panel1c-header"
+                      >
+                        <div className={classes.column}>
+                          <Typography className={classes.heading}>Location</Typography>
+                        </div>
+                        <div className={classes.column}>
+                          <Typography className={classes.secondaryHeading}>Select trip destination</Typography>
+                        </div>
+                      </ExpansionPanelSummary>
+                      <ExpansionPanelDetails className={classes.details}>
+                        <div className={classes.column} />
+                        <div className={classes.column}>
+                          <Chip label="Barbados" onDelete={() => {}} />
+                        </div>
+                        <div className={clsx(classes.column, classes.helper)}>
+                          <Typography variant="caption">
+                            Select your destination of choice
+                            <br />
+                            <a href="#secondary-heading-and-columns" className={classes.link}>
+                              Learn more
+                            </a>
+                          </Typography>
+                        </div>
+                      </ExpansionPanelDetails>
+                      <Divider />
+                      <ExpansionPanelActions>
+                        <Button size="small">Cancel</Button>
+                        <Button size="small" color="primary">
+                          Save
+                        </Button>
+                      </ExpansionPanelActions>
+                    </ExpansionPanel>
+                  </div>
+
                 </TabPanel>
                 <TabPanel value={value} index={1}>
                   Item Two
